58633adf082a244561dc12de36d9947c805f202d
22smetana <mrswegy@gmail.com>
2025-10-31 09:13:15 +0100

    2. commit - vytvorena trida BankovniUcet a objekt

--- src/smetanasps/cz/Main.java
+++ src/smetanasps/cz/Main.java
@@ -2,6 +2,8 @@ package smetanasps.cz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projekt na zacatku v1");
+        System.out.println("Projekt se tridou BankovniUcet v2");
+
+        BankovniUcet bu01 = new BankovniUcet("127-2566771/0100", "Jan Novak", 0);
     }
 }
